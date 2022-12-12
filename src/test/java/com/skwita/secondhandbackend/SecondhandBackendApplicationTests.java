317f2d4fdef583c7859fbeddc232ff5d34204273
Max <skwita.max@gmail.com>
2022-12-12 16:09:56 +0300

    basic project structure

---
+++ src/test/java/com/skwita/secondhandbackend/SecondhandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwita.secondhandbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondhandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

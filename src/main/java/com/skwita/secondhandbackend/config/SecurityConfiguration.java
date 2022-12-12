eba5d430d9a03ffb811762f02d605b86401f0dc2
Max <skwita.max@gmail.com>
2022-12-12 17:30:58 +0300

    deleted unused things

--- src/main/java/com/skwita/secondhandbackend/config/SecurityConfiguration.java
+++ src/main/java/com/skwita/secondhandbackend/config/SecurityConfiguration.java
@@ -4,18 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
